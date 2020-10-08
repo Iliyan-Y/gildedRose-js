552987d9b8182b164cbc26861868f9984125954b
Iliyan-Y <maximizebg@gmail.com>
2020-10-08 15:17:59 +0100

    Test for normal items added

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,2 +1,16 @@
 var { Shop, Item } = require('../src/gilded_rose.js');
-describe('Gilded Rose', function () {});
+var { FakeItem } = require('../spec/testHelpers');
+describe('Gilded Rose', function () {
+  describe('Normal Items', () => {
+    let shop;
+    beforeEach(() => {
+      shop = new Shop([new FakeItem('+5 Dexterity Vest', 10, 20)]);
+    });
+
+    it('Check if the quality and sellIn properties work', () => {
+      shop.updateQuality();
+      expect(shop.items[0].sellIn).toEqual(9);
+      expect(shop.items[0].quality).toEqual(19);
+    });
+  });
+});
--- spec/testHelpers.js
+++ spec/testHelpers.js
@@ -1,7 +1,10 @@
-class fakeItem {
+class FakeItem {
   constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
 }
+module.exports = {
+  FakeItem,
+};
